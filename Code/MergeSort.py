aadc959bf55a626ac84e10c1297a124be9b0a3f5
briefisa <127897889+briefisa@users.noreply.github.com>
2023-05-08 21:45:38 +0000

    merge sort (recursive) in python

--- Code/MergeSort.py
+++ Code/MergeSort.py
@@ -1,30 +1,43 @@
-from random import*
-import math
+import random, resource, math, sys
 
-def merge_sort(list):
-    if list.len() == 1:
-        return list
-    int:len = list.len()
+resource.setrlimit(resource.RLIMIT_STACK, (2**29, -1))
+sys.setrecursionlimit(10**6)
+                      
+def merge_sort(wordList):
+    if len(wordList) <= 1:
+        return wordList
+    length = len(wordList)
     first = []
     second = []
-    for x in range(math.floor(len/2)):
-        first[x] = list[x]
-    for x in range(math.floor(len/2) + 1, list.len() + 1):
-        second[x] = list[x]
+    for x in range(length//2):
+        first.append(wordList[x])
+    for x in range(length//2, length):
+        second.append(wordList[x])
     first = merge_sort(first)
     second = merge_sort(second)
     output = []
     a = 0
     b = 0
-    while a < first.len() and b < second.len():
+    while a < len(first) and b < len(second):
         if first[a] <= second[b]:
             output.append(first[a])
-            a + 1
+            a += 1
         elif first[a] > second[b]:
             output.append(second[b])
-            b + 1
+            b += 1
+    while a < len(first):
+        output.append(first[a])
+        a += 1
+    while b < len(second):
+        output.append(second[b])
+        b += 1
+    return output
 
 array = []
 for x in range(100):
     array.append(random.randint(0, 100))
-merge_sort(list)
+print(len(array))
+array = merge_sort(array)
+for a in array:
+    print(a)
+print("length: ", len(array))
