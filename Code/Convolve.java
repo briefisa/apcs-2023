1818fee8d7a7e9b24fe1c0ba1946a2a0f7737e6d
briefisa <127897889+briefisa@users.noreply.github.com>
2023-05-25 21:46:08 +0000

    its a start

--- Code/Convolve.java
+++ Code/Convolve.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
 
 public class Convolve {
-    
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public static ArrayList<Integer> dumbConvolve(int[] a, int[] b) {
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        for(int i = 0; i < a.length; i++) {
+            for(int j = 0; j < b.length; j++) {
+                 
+            }
+        }
+    }
 }
