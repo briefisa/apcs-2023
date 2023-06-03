d5f7bc1a8fbed2e5b932f485fe1c475e7d2ef228
briefisa <127897889+briefisa@users.noreply.github.com>
2023-06-03 03:42:51 +0000

    progress

--- Code/Convolve.java
+++ Code/Convolve.java
@@ -1,16 +1,29 @@
 import java.util.ArrayList;
+import java.awt.Graphics;
 
 public class Convolve {
     public static void main(String[] args) {
-        
+        int[] a = {1, 2, 3, 4};
+        int[] b = {1, 2};
+        int[] c = dumbConvolve(a, b);
+        String str = "";
+        for (int num : c) {
+            str = str + num + ", ";
+        }
+        System.out.print("[" + str.substring(0, str.length() - 2) + "]");
     }
 
-    public static ArrayList<Integer> dumbConvolve(int[] a, int[] b) {
-        ArrayList<Integer> output = new ArrayList<Integer>();
-        for(int i = 0; i < a.length; i++) {
-            for(int j = 0; j < b.length; j++) {
-                 
+    public static int[] dumbConvolve(int[] a, int[] b) {
+        int[] output = new int[a.length + b.length - 1];
+        for(int i = 0; i < a.length + b.length - 1; i++) {
+            int num = 0;
+            for(int j = i; j >= 0 && i - j < b.length; j--) {
+                if (j < a.length) {
+                    num += a[j]*b[i - j];
+                }
             }
+            output[i] = num;
         }
+        return output;
     }
 }
--- Code/Convolve.py
+++ Code/Convolve.py
@@ -1,3 +1,17 @@
+import sympy as sp
 
-def dumb_convolve(a: list, b: list) -> list:
-    for 
+def discrete_conv(a: list, b: list) -> list:
+    output = []
+    for i in range(0, len(a) + len(b) - 1):
+        num = 0
+        j = 0
+        while j < len(b) and (i - j) >= 0:
+            if (i - j) < len(a):
+                num += a[i - j]*b[j]
+            j += 1
+        output.append(num)
+    return output
+
+one = [1,2,3,4]
+two = [1,2]
+print(discrete_conv(two, one))
