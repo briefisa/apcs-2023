658599c83c47f852a7c151ae47b1490fb6f08bd7
briefisa <127897889+briefisa@users.noreply.github.com>
2023-06-05 05:04:46 +0000

    somewhat there

--- Code/Convolve.py
+++ Code/Convolve.py
@@ -1,4 +1,4 @@
-import sympy as sp
+import math, cmath
 
 def discrete_conv(a: list, b: list) -> list:
     output = []
@@ -12,6 +12,52 @@ def discrete_conv(a: list, b: list) -> list:
         output.append(num)
     return output
 
-one = [1,2,3,4]
-two = [1,2]
-print(discrete_conv(two, one))
+def fft(P: list) -> list:
+    n = len(P)
+    if n == 1:
+        return n
+    w = complex(math.cos((2*math.pi)/n), math.sin((2*math.pi)/n))
+    Pe, Po = P[::2], P[1::2]
+    ye, yo = fft(Pe), fft(Po)
+    y = [0]*n
+    if type(ye) == int:
+        for j in range(n//2):
+            y[j] = ye + (pow(w, j))*yo
+            y[j + n//2] = ye - (pow(w, j))*yo
+    else:
+        for j in range(n//2):
+            y[j] = ye[j] + (pow(w, j))*yo[j]
+            y[j + 1] = ye[j] - (pow(w, j))*yo[j]
+    return y
+
+def ifft(P):
+    n = len(P)
+    if n == 1:
+        return n
+    w = (1/n)*complex(math.cos((2*math.pi)/n), math.sin((2*math.pi)/n))
+    Pe, Po = P[::2], P[1::2]
+    ye, yo = ifft(Pe), ifft(Po)
+    y = [0]*n
+    if type(ye) == int:
+        for j in range(n//2):
+            y[j] = ye + (pow(w, j))*yo
+            y[j + n//2] = ye - (pow(w, j))*yo
+    else:
+        for j in range(n//2):
+            y[j] = ye[j] + (pow(w, j))*yo[j]
+            y[j + n//2] = ye[j] - (pow(w, j))*yo[j]
+    return y
+
+
+#def func1(x: float):
+#    return x
+
+#def func2(x: float):
+#    return 0
+
+a = [5, 1, 3, 2]
+print(fft(a))
+
+
+
+
